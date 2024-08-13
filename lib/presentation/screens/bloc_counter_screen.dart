e191fbcd4fde2198b5ba8ce2c7de0be66b058a2d
JaViKo500 <javikogutierrez64@gmail.com>
2024-08-12 23:51:06 -0500

    call bloc counter

--- lib/presentation/screens/bloc_counter_screen.dart
+++ lib/presentation/screens/bloc_counter_screen.dart
@@ -1,31 +1,41 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:forms_app/presentation/blocs/counter_bloc/counter_bloc.dart';
 
 class BlockCounterScreen extends StatelessWidget {
+  const BlockCounterScreen({super.key});
 
-  const BlockCounterScreen ({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => CounterBloc(),
+      child: const _BlocCounterView(),
+    );
+  }
+}
 
+class _BlocCounterView extends StatelessWidget {
+  const _BlocCounterView();
+  void increaseCounterBy( BuildContext context, [ int value = 1 ] ) {
+     context.read<CounterBloc>().add( CounterIncreased( value ) );
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text(
-          'Block Counter Screen',
-          style: TextStyle()
-        ),
+        title: const Text('Block Counter Screen', style: TextStyle()),
         actions: [
           IconButton(
-            onPressed: () {
-              
-            }, 
+            onPressed: () {},
             icon: const Icon(Icons.refresh_rounded),
           ),
         ],
       ),
-      body: const Center(
-        child: Text(
-          'Counter value',
-          style: TextStyle()
+      body: Center(
+        child: context.select(
+          (CounterBloc counterBloc) => Text(
+              'Counter value ${counterBloc.state.counter}',
+              style: const TextStyle()),
         ),
       ),
       floatingActionButton: Column(
@@ -34,35 +44,24 @@ class BlockCounterScreen extends StatelessWidget {
         children: [
           FloatingActionButton(
             heroTag: '1',
-            onPressed: () {
-              
-            },
-            child: const Text(
-              '+3',
-              style: TextStyle()
-            ),
+            onPressed: () => increaseCounterBy( context, 3 ),
+            child: const Text('+3', style: TextStyle()),
           ),
-          const SizedBox(height: 15,),
+          const SizedBox(
+            height: 15,
+          ),
           FloatingActionButton(
             heroTag: '2',
-            onPressed: () {
-              
-            },
-            child: const Text(
-              '+2',
-              style: TextStyle()
-            ),
+            onPressed: () => increaseCounterBy( context, 2 ),
+            child: const Text('+2', style: TextStyle()),
           ),
-          const SizedBox(height: 15,),
+          const SizedBox(
+            height: 15,
+          ),
           FloatingActionButton(
             heroTag: '3',
-            onPressed: () {
-              
-            },
-            child: const Text(
-              '+1',
-              style: TextStyle()
-            ),
+            onPressed: () => increaseCounterBy( context ),
+            child: const Text('+1', style: TextStyle()),
           ),
         ],
       ),
